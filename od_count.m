8e08d87a7f47515f7d61c7887988dc6be787f01c
SARAHP\sarab <s.betancur.giraldo@student.tue.nl>
2024-12-18 10:30:02 -0500

    Destinations data

--- od_count.m
+++ od_count.m
@@ -11,7 +11,34 @@ Tmax = 20/60;
 od_acc = R_selector * (tfast<Tmax)';
 od_region = sum(R_selector,2);
 
-[pc_unique od_region od_acc]
+% n_regions = size(R_selector,1);
+OD_framework = {};
+
+for i = 1:4
+    if i == 1
+        load("output/AFI_heatmap_minTT.mat");
+    elseif i == 2
+        load("output/AFI_heatmap_avgAcc.mat");
+    elseif i == 3
+        load("output/AFI_heatmap_pathAcc.mat");
+    elseif i == 4
+        load("output/AFI_heatmap_pathAccMILP.mat");
+    end
+
+    b_OD = zeros(nOD,1); b_OD(find(~AFI_epsilons)) = 1; 
+    OD_framework{1,i} = R_selector*b_OD;
+
+end
+
+dest_data = [pc_unique od_region od_acc ];
+
+T = table(dest_data(:,1),dest_data(:,2),dest_data(:,3), ...
+          OD_framework{1,1},OD_framework{1,2},OD_framework{1,3}, ...
+          OD_framework{1,4},population_region, ...
+          'VariableNames', ...
+          ["PC","OD desired","OD shortPath", ...
+          "OD minTT","OD commAcc","OD tripAcc","OD destAcc", ...
+          "Population"]);
 
 
 
