b38787d53201d0c09c5a55b35f971e8b2405771d
SARAHP\sarab <s.betancur.giraldo@student.tue.nl>
2025-06-17 14:28:14 +0200

    fix consolidation for 0-2, 4, 4-5 nCar runs

--- consolidate_Js.m
+++ consolidate_Js.m
@@ -5,28 +5,33 @@ close all; clear; clc;
 load('model/data_g.mat');
 load("model/data_shortPaths.mat");
 
-J_03 = load('output/J.mat');
+J_02 = load('output/J_02.mat');
+J_3 = load('output/J_3.mat');
 J_45 = load('output/J_45.mat');
 
-UtilEff = zeros(size(J_03.UtilEff));
-CommSuff = zeros(size(J_03.CommSuff)); 
-TripSuff = zeros(size(J_03.TripSuff)); 
-AccSuff = zeros(size(J_03.AccSuff)); 
+UtilEff = zeros(size(J_02.UtilEff));
+CommSuff = zeros(size(J_02.CommSuff)); 
+TripSuff = zeros(size(J_02.TripSuff)); 
+AccSuff = zeros(size(J_02.AccSuff)); 
 
 
-UtilEff(:,1:3,:) = J_03.UtilEff;
+UtilEff(:,1:2,:) = J_02.UtilEff;
+UtilEff(:,3,:) = J_3.UtilEff;
 UtilEff(:,4:5,:) = J_45.UtilEff;
 
 
-CommSuff(:,1:3,:) = J_03.CommSuff;
+CommSuff(:,1:2,:) = J_02.CommSuff;
+CommSuff(:,3,:) = J_3.CommSuff;
 CommSuff(:,4:5,:) = J_45.CommSuff;
 
 
-TripSuff(:,1:3,:) = J_03.TripSuff;
+TripSuff(:,1:2,:) = J_02.TripSuff;
+TripSuff(:,3,:) = J_3.TripSuff;
 TripSuff(:,4:5,:) = J_45.TripSuff;
 
 
-AccSuff(:,1:3,:) = J_03.AccSuff;
+AccSuff(:,1:2,:) = J_02.AccSuff;
+AccSuff(:,3,:) = J_3.AccSuff;
 AccSuff(:,4:5,:) = J_45.AccSuff;
 
 
